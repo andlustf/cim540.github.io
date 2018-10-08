1d827771d596984e0ab75f500f11747f3a00bfe7
andrealustgarten <andlustf@gmail.com>
2018-10-08 12:25:21 -0400

    homework week 7
    done

--- homework/week7/sketch.js
+++ homework/week7/sketch.js
@@ -1,8 +1,29 @@
-function setup () {
-  // put setup code here
-  createCanvas (480, 120);
-  background (204);
+var street;
+var dog;
+var bike;
+
+function preload() {
+  street = loadImage("street.jpg");
+  dog = loadImage("dog.png");
+  bike = loadImage("bike.png");
 }
 
-function draw () {
+function setup() {
+  createCanvas(1000, 581);
+}
+
+function draw() {
+  background (0);
+  tint (255);
+  image (street, 0, 0, 1032, 580.5);
+  tint (255);
+  image (dog, 600, 350,300,150);
+  tint (0, 255, 0);
+  image (dog, 450, 350,300,150);
+  tint (255, 0, 0);
+  image (dog, 250, 350,300,150);
+  tint (0, 0, 255);
+  image (dog, 50, 350,300,150);
+  tint (255);
+  image (bike, mouseX *1, 340, 246, 256);
 }
