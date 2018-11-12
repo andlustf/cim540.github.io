bb90be14b7e5452f143ec44a52b11a3b273a5dd3
Andrea Lustgarten <axl452@miami.edu>
2018-11-12 15:52:54 -0500

    homework
    update

--- homework/week11/sketch.js
+++ homework/week11/sketch.js
@@ -1,38 +1,53 @@
 //MOAI CODE
-/*   rect (40, 40, 80, 160);
+/*  rect (40, 40, 80, 160);
   ellipse (65, 90, 20, 70);
   ellipse (100, 65, 20, 20);
   line (120, 160, 80, 160);*/
 
 //constructor for moai
-function Moai (initX, initY, size, color) {
-  // properties: location (x, y)
-  this.x = initX;
-  this.y = initY;
-  this.size = size;
-  this.color = color;
-  // methods: buildmoai ()
-  this.buildmoai = function () {
-    rect (this.x, this.y, this.size, this.size*2);
-    ellipse (this.x+this.y/8, this.y+this.size, 20, 70);
-    ellipse (this.x+60, this.y+25, this.size/4, this.size/4);
-    line (this.x+80, this.y+120, this.size, this.size*2);
-  }
+function Moai(initX, initY, size){
+  this.x = initX;
+  this.y = initY;
+  this.size = size;
+  this.buildmoai = function(){
+    fill(179, 179, 179);
+    rect(this.x, this.y, this.size, this.size*2);
+    fill(89, 89, 89);
+    ellipse(this.x+this.size*.3, this.y+this.size*.8, this.size*.35, this.size*1.4);
+    ellipse(this.x+this.size*.7, this.y+this.size*.4, this.size/4, this.size/4);
+    triangle (this.x+this.size, this.y+this.size*4, this.x+this.size, this.y+this.x/2, this.x+this.size*1.2, this.y*1.6);
+    //triangle(this.x+this.size, this.y+this.x, this.x+this.size, this.y+this.x*3, this.x+this.size*1.2, this.y+this.x*3);
+    line (this.x-this.size*.2, this.y*4, this.x*2, this.y+4);
+  }
 }
 
-var moai1;
-var moai2;
-var moai3;
+var grass;
+var M1;
+var M2;
+var M3;
+var M4;
+var M5;
 
-function setup () {
-  createCanvas (480, 240);
+function preload() {
+  grass = loadImage("moai.jpeg");
+}
+
+function setup(){
+  createCanvas(828, 549);
   background (204);
-  angleMode (DEGREES);
-  moai1 = new Moai (40, 40, 80, 160);
-  moai2 = new Moai (160, 40, 80);
+  noStroke ();
+  M1 = new Moai(50, 275, 125);
+  M2 = new Moai(200, 275, 100);
+  M3 = new Moai(325, 275, 75);
+  M4 = new Moai(425, 275, 50);
+  M5 = new Moai(500, 275, 25);
 }
 
-function draw () {
-  moai1.buildmoai ();
-  moai2.buildmoai ();
+function draw(){
+  image(grass, 0, 0, 828, 549);
+  M1.buildmoai();
+  M2.buildmoai();
+  M3.buildmoai();
+  M4.buildmoai();
+  M5.buildmoai();
 }
